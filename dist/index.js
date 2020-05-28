58e50ef86efd4262bd2469559dbc3088c32c1fef
cambiph <philippe.cambien@gmail.com>
2020-05-28 16:25:07 +0200

    Fix rode build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    VlCookieConsent: require('../dist/vl-cookie-consent.src.js').VlCookieConsent,
-    VlCookieConsentOptIn: require('../dist/vl-cookie-consent-opt-in.src.js').VlCookieConsentOptIn
-}
+  VlCookieConsent: require('../dist/vl-cookie-consent.src.js').VlCookieConsent,
+  VlCookieConsentOptIn: require('../dist/vl-cookie-consent-opt-in.src.js').VlCookieConsentOptIn,
+};
--- test/e2e/pages/vl-cookie-consent.page.js
+++ test/e2e/pages/vl-cookie-consent.page.js
@@ -1,84 +1,83 @@
 const VlCookieConsent = require('../components/vl-cookie-consent');
-const { Page, Config } = require('vl-ui-core').Test;
-const { By, until } = require('selenium-webdriver');
+const {Page, Config} = require('vl-ui-core').Test;
+const {By, until} = require('selenium-webdriver');
 
 class VlCookieConsentPage extends Page {
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-cookie-consent.html');
-    }
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-cookie-consent.html');
+  }
 
-    async _getConsent(selector) {
-        const element = await this.driver.findElement(By.css(selector));
-        return new VlCookieConsent(this.driver, element);
-    }
+  async _getConsent(selector) {
+    const element = await this.driver.findElement(By.css(selector));
+    return new VlCookieConsent(this.driver, element);
+  }
 
-    async _openConsent(selector) {
-        const consent = await this.driver.findElement(By.css(selector));
-        await this.driver.wait(until.elementIsVisible(consent), 3000);
-        await this.driver.wait(until.elementIsEnabled(consent), 3000);
-        await consent.click();
-    }
+  async _openConsent(selector) {
+    const consent = await this.driver.findElement(By.css(selector));
+    await this.driver.wait(until.elementIsVisible(consent), 3000);
+    await this.driver.wait(until.elementIsEnabled(consent), 3000);
+    await consent.click();
+  }
 
-    async getConsent() {
-        return this._getConsent('#cookie-consent');
-    }
+  async getConsent() {
+    return this._getConsent('#cookie-consent');
+  }
 
-    async getConsentWithoutFunctionalOptIn() {
-        return this._getConsent('#cookie-consent-without-functional-opt-in');
-    }
+  async getConsentWithoutFunctionalOptIn() {
+    return this._getConsent('#cookie-consent-without-functional-opt-in');
+  }
 
-    async getConsentWithExtraOptIn() {
-        return this._getConsent('#cookie-consent-with-extra-opt-in');
-    }
+  async getConsentWithExtraOptIn() {
+    return this._getConsent('#cookie-consent-with-extra-opt-in');
+  }
 
-    async getConsentWithExtraCheckedOptIn() {
-        return this._getConsent('#cookie-consent-with-extra-checked-opt-in');
-    }
+  async getConsentWithExtraCheckedOptIn() {
+    return this._getConsent('#cookie-consent-with-extra-checked-opt-in');
+  }
 
-    async getConsentWithExtraMandatoryOptIn() {
-        return this._getConsent('#cookie-consent-with-extra-mandatory-opt-in');
-    }
+  async getConsentWithExtraMandatoryOptIn() {
+    return this._getConsent('#cookie-consent-with-extra-mandatory-opt-in');
+  }
 
-    async getConsentWithExtraDynamicOptIn() {
-        return this._getConsent('#cookie-consent-with-extra-dynamic-opt-in');
-    }
+  async getConsentWithExtraDynamicOptIn() {
+    return this._getConsent('#cookie-consent-with-extra-dynamic-opt-in');
+  }
 
-    async getConsentWithDynamicText() {
-        return this._getConsent('#cookie-consent-with-custom-text');
-    }
+  async getConsentWithDynamicText() {
+    return this._getConsent('#cookie-consent-with-custom-text');
+  }
 
-    async openConsent() {
-        await this._openConsent('#button-open-cookie-consent');
-    }
+  async openConsent() {
+    await this._openConsent('#button-open-cookie-consent');
+  }
 
-    async openConsentWithoutFunctionalOptIn() {
-        await this._openConsent('#button-open-cookie-consent-without-functional-opt-in');
-    }
+  async openConsentWithoutFunctionalOptIn() {
+    await this._openConsent('#button-open-cookie-consent-without-functional-opt-in');
+  }
 
-    async openConsentWithExtraOptIn() {
-        await this._openConsent('#button-open-cookie-consent-with-extra-opt-in');
-    }
+  async openConsentWithExtraOptIn() {
+    await this._openConsent('#button-open-cookie-consent-with-extra-opt-in');
+  }
 
-    async openConsentWithExtraCheckedOptIn() {
-        await this._openConsent('#button-open-cookie-consent-with-extra-checked-opt-in');
-    }
+  async openConsentWithExtraCheckedOptIn() {
+    await this._openConsent('#button-open-cookie-consent-with-extra-checked-opt-in');
+  }
 
-    async openConsentWithExtraMandatoryOptIn() {
-        await this._openConsent('#button-open-cookie-consent-with-extra-mandatory-opt-in');
-    }
+  async openConsentWithExtraMandatoryOptIn() {
+    await this._openConsent('#button-open-cookie-consent-with-extra-mandatory-opt-in');
+  }
 
-    async openConsentWithExtraDynamicOptIn() {
-        await this._openConsent('#button-open-cookie-consent-with-extra-dynamic-opt-in');
-    }
+  async openConsentWithExtraDynamicOptIn() {
+    await this._openConsent('#button-open-cookie-consent-with-extra-dynamic-opt-in');
+  }
 
-    async openConsentWithDynamicText() {
-        await this._openConsent('#button-open-cookie-consent-with-custom-text');
-    }
+  async openConsentWithDynamicText() {
+    await this._openConsent('#button-open-cookie-consent-with-custom-text');
+  }
 
-    async voegSocialeMediaOptInToe() {
-        await (await this.driver.findElement(By.css('#button-add-cookie-consent-opt-in'))).click();
-    }
-
+  async voegSocialeMediaOptInToe() {
+    await (await this.driver.findElement(By.css('#button-add-cookie-consent-opt-in'))).click();
+  }
 }
 
 module.exports = VlCookieConsentPage;
